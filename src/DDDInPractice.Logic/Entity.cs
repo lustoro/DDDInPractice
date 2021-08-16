1f5468617ac8e6d6956c31daea021c5a5af967ed
Lusine Torosyan <lusine.torosyan@optym.com>
2021-08-16 06:22:53 +0400

    Created Entity for Snack Machine.

---
+++ src/DDDInPractice.Logic/Entity.cs
@@ -0,0 +1,42 @@
+﻿namespace DDDInPractice.Logic
+{
+   public abstract class Entity
+    {
+        public long Id { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+            if (ReferenceEquals(other, null))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            if (GetType() != other.GetType())
+                return false;
+            if (Id == 0 || other.Id == 0)
+                return false;
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity entity1, Entity entity2)
+        {
+            if (ReferenceEquals(entity1, null) && ReferenceEquals(entity2, null))
+                return true;
+            if (ReferenceEquals(entity1, null) || ReferenceEquals(entity2, null))
+                return false;
+
+            return entity1.Equals(entity2);
+        }
+
+        public static bool operator !=(Entity entity1, Entity entity2)
+        {
+            return !(entity1 == entity2);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
+
+    }
+}
