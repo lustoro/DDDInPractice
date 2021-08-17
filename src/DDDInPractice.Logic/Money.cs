5135b6b7f38a77cb4b7e7ba4d779302efb2ca8a4
Lusine Torosyan <lusine.torosyan@optym.com>
2021-08-17 23:04:10 +0400

    Added Unit tests for Snack Machine entity and Money value object.

--- src/DDDInPractice.Logic/Money.cs
+++ src/DDDInPractice.Logic/Money.cs
@@ -1,13 +1,32 @@
-﻿namespace DDDInPractice.Logic
+﻿using System;
+
+namespace DDDInPractice.Logic
 {
    public sealed class Money: ValueObject<Money>
     {
-        public int OneCentCount { get; set; }
-        public int TenCentCount { get; set; }
-        public int QuarterCount { get; set; }
-        public int OneDollarCount { get; set; }
-        public int FiveDollarCount { get; set; }
-        public int TwentyDollarCount { get; set; }
+        public static readonly Money None = new Money(0, 0, 0, 0, 0, 0);
+        public static readonly Money Cent = new Money(1, 0, 0, 0, 0, 0);
+        public static readonly Money TenCent = new Money(0, 1, 0, 0, 0, 0);
+        public static readonly Money Quarter = new Money(0, 0, 1, 0, 0, 0);
+        public static readonly Money Dollar = new Money(0, 0, 0, 1, 0, 0);
+        public static readonly Money FiveDollar = new Money(0, 0, 0, 0, 1, 0);
+        public static readonly Money TwentyDollar = new Money(0, 0, 0, 0, 0, 1);
+
+        public int OneCentCount { get; }
+        public int TenCentCount { get; }
+        public int QuarterCount { get; }
+        public int OneDollarCount { get; }
+        public int FiveDollarCount { get; }
+        public int TwentyDollarCount { get; }
+
+        public decimal Amount => 
+                       OneCentCount * 0.01m +
+                       TenCentCount * 0.10m +
+                       QuarterCount * 0.25m +
+                       OneDollarCount +
+                       FiveDollarCount * 5 +
+                       TwentyDollarCount * 20;
+
 
         public Money(int oneCentCount,
                      int tenCentCount,
@@ -16,6 +35,19 @@
                      int fiveDollarCount,
                      int twnetyDollarCount)
         {
+            if (oneCentCount < 0)
+                throw new InvalidOperationException();
+            if (tenCentCount < 0)
+                throw new InvalidOperationException();
+            if (quarterCount < 0)
+                throw new InvalidOperationException();
+            if (oneDollarCount < 0)
+                throw new InvalidOperationException();
+            if (fiveDollarCount < 0)
+                throw new InvalidOperationException();
+            if (twnetyDollarCount < 0)
+                throw new InvalidOperationException();
+
             OneCentCount = oneCentCount;
             TenCentCount = tenCentCount;
             QuarterCount = quarterCount;
@@ -34,6 +66,18 @@
                 money1.FiveDollarCount + money2.FiveDollarCount,
                 money1.TwentyDollarCount + money2.TwentyDollarCount);
             return sum;
+        }
+
+        public static Money operator -(Money money1, Money money2)
+        {
+            return new Money(
+                money1.OneCentCount - money2.OneCentCount,
+                money1.TenCentCount - money2.TenCentCount,
+                money1.QuarterCount - money2.QuarterCount,
+                money1.OneDollarCount - money2.OneDollarCount,
+                money1.FiveDollarCount - money2.FiveDollarCount,
+                money1.TwentyDollarCount - money2.TwentyDollarCount
+                );
         }
 
         protected override bool EqualsCore(Money other)
--- src/DDDInPractice.Logic/SnackMachine.cs
+++ src/DDDInPractice.Logic/SnackMachine.cs
@@ -1,27 +1,33 @@
-﻿namespace DDDInPractice.Logic
+﻿using System;
+using System.Linq;
+using static DDDInPractice.Logic.Money;
+
+namespace DDDInPractice.Logic
 {
     public sealed class SnackMachine: Entity
     {
-        public Money MoneyInside { get; private set; }
-        public Money MoneyInTransaction { get; private set; }       
+        public Money MoneyInside { get; private set; } = None;
+        public Money MoneyInTransaction { get; private set; } = None;
 
 
-        
         public void InsertMoney(Money money)
         {
+            Money[] coinsAndNotes = {Cent, TenCent, Quarter, Dollar, FiveDollar, TwentyDollar };
+            if (!coinsAndNotes.Contains(money))
+                throw new InvalidOperationException();
             MoneyInTransaction += money;
         }
 
         public void ReturnMoney()
         {
-            //MoneyInTransaction = 0;
+            MoneyInTransaction = None;
         }
 
         public void BuySnack()
         {
             MoneyInside += MoneyInTransaction;
 
-            //MoneyInTransaction = 0;
+            MoneyInTransaction = None;
         }
     }
 }
